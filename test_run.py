e671e335dbf257c2f3f82c6c46ceb913787d2df6
albertleb69@yandex.ru <Pointnew690>
2022-12-02 23:22:49 +0300

    Init

--- test_run.py
+++ test_run.py
@@ -1,14 +1,15 @@
 import allure
 
 
-@allure.title('First test')
-@allure.description('First test')
-def test_first():
-    with allure.step('Ассерт'):
-        assert 1 == 1, "Ошибка"
+@allure.suite(suite_name='Тесты для Jenkins')
+class TestJenkins:
+    @allure.title('First test')
+    @allure.description('First test')
+    def test_first(self):
+        with allure.step('Ассерт'):
+            assert 1 == 1, "Ошибка"
 
-
-@allure.description('Second test')
-def test_second():
-    with allure.step('Second test'):
-        assert 2 == 2, "Ошибка"
+    @allure.description('Second test')
+    def test_second(self):
+        with allure.step('Second test'):
+            assert 2 == 2, "Ошибка"
