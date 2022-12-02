8e4f5e27e12a29d9cdee7f13d6e7ef57bf96fb6c
albertleb69@yandex.ru <Pointnew690>
2022-12-02 23:20:13 +0300

    Init

--- test_run.py
+++ test_run.py
@@ -1,8 +1,14 @@
 import allure
 
 
-@allure.description('Тестирование Яндекса')
-def test_yandex():
+@allure.title('First test')
+@allure.description('First test')
+def test_first():
     with allure.step('Ассерт'):
         assert 1 == 1, "Ошибка"
 
+
+@allure.description('Second test')
+def test_second():
+    with allure.step('Second test'):
+        assert 2 == 2, "Ошибка"
