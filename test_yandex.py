1f6defe3c180f80706dfa1b1c6f594c470064c19
albertleb69@yandex.ru <Pointnew690>
2022-12-04 20:19:08 +0300

    Init

--- test_yandex.py
+++ test_yandex.py
@@ -10,17 +10,21 @@ class TestJenkins:
     @allure.description('First test')
     def test_yandex(self):
         with allure.step('Ассерт'):
-            capabilities = {
-                "browserName": "firefox",
-                "browserVersion": "106.0",
-                "selenoid:options": {
-                    "enableVNC": True,
-                    "enableVideo": False
+            driver = None
+            try:
+                capabilities = {
+                    "browserName": "firefox",
+                    "browserVersion": "106.0",
+                    "selenoid:options": {
+                        "enableVNC": True,
+                        "enableVideo": False
+                    }
                 }
-            }
-            driver = webdriver.Remote(
-                command_executor="http://localhost:4444//wd/hub",
-                desired_capabilities=capabilities
-            )
-            driver.get('https://ya.ru')
-            time.sleep(5)
+                driver = webdriver.Remote(
+                    command_executor="http://localhost:4444//wd/hub",
+                    desired_capabilities=capabilities
+                )
+                driver.get('https://ya.ru')
+            finally:
+                driver.quit()
+
